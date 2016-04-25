fcfa15a29a2d0b2c7c14d46d2935968b06498ac8
Nishant Srivastava <nisrulz@gmail.com>
2016-04-25 12:51:01 +0530

    [*] refactoring and added searchForTweets()

--- src/github/nisrulz/bot/TwitterBot.java
+++ src/github/nisrulz/bot/TwitterBot.java
@@ -2,6 +2,8 @@ package github.nisrulz.bot;
 
 import java.util.List;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -12,52 +14,64 @@ public class TwitterBot {
 	static boolean debug = false;
 
 	// if something goes wrong, we might see a TwitterException
-	public static void main(String... args) throws TwitterException {
+	public static void main(String... args) {
 		// send a tweet
 		if (!debug) {
-			// Send Tweet
-			// sendTweet("Hello World!");
-			
-			// Get tweets from Home Timeline
-			getHomeTimeLine();
+			try {
+				// Send Tweet
+				// sendTweet("Hello World!");
 
+				// Get tweets from Home Timeline
+				// getHomeTimeLine();
+
+				// Seach for tweets
+				searchForTweets("@gdg_nd");
+			} catch (TwitterException e) {
+				e.printStackTrace();
+			}
 		} else {
 			// print a message so we know when it finishes
-			System.out.println("Successfull");
+			System.out.println("Debug Mode Enabled!");
 		}
 	}
 
-	private static Status sendTweet(String text) {
+	private static Status sendTweet(String text) throws TwitterException {
 		// access the twitter API using your twitter4j.properties file
 		// The factory instance is re-useable and thread safe.
 		Twitter twitter = TwitterFactory.getSingleton();
 
 		Status status = null;
-		try {
-			status = twitter.updateStatus(text);
-			System.out.println("Successfully updated the status to ["
-					+ status.getText() + "].");
-		} catch (TwitterException e) {
-			e.printStackTrace();
-		}
+
+		status = twitter.updateStatus(text);
+		System.out.println("Successfully updated the status to ["
+				+ status.getText() + "].");
 
 		return status;
 	}
 
-	private static void getHomeTimeLine() {
+	private static void getHomeTimeLine() throws TwitterException {
 		Twitter twitter = TwitterFactory.getSingleton();
 		List<Status> statuses = null;
-		try {
-			statuses = twitter.getHomeTimeline();
-		} catch (TwitterException e) {
-			e.printStackTrace();
-		}
+		statuses = twitter.getHomeTimeline();
+
 		System.out.println("Showing home timeline.");
 		if (statuses != null) {
 			for (Status status : statuses) {
 				System.out.println(status.getUser().getName() + ":"
 						+ status.getText());
 			}
+		}
+	}
+
+	private static void searchForTweets(String query_text)
+			throws TwitterException {
+		Twitter twitter = TwitterFactory.getSingleton();
+		Query query = new Query(query_text);
+		QueryResult result;
+		result = twitter.search(query);
+		for (Status status : result.getTweets()) {
+			System.out.println("@" + status.getUser().getScreenName() + ":"
+					+ status.getText());
 		}
 	}
 }
