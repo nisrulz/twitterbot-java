e1f52e4084f8d9c7620637963edbe2564b8192f8
Nishant Srivastava <nisrulz@gmail.com>
2016-04-25 12:40:38 +0530

    [*] code refactor and added getHomeTimeline()

--- src/github/nisrulz/bot/TwitterBot.java
+++ src/github/nisrulz/bot/TwitterBot.java
@@ -1,5 +1,7 @@
 package github.nisrulz.bot;
 
+import java.util.List;
+
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -8,32 +10,54 @@ import twitter4j.TwitterFactory;
 public class TwitterBot {
 
 	static boolean debug = false;
-	static Twitter twitter;
 
 	// if something goes wrong, we might see a TwitterException
 	public static void main(String... args) throws TwitterException {
-
-		// access the twitter API using your twitter4j.properties file
-		twitter = TwitterFactory.getSingleton();
-
 		// send a tweet
 		if (!debug) {
-			sendTweet(twitter, "Hello World!");
+			// Send Tweet
+			// sendTweet("Hello World!");
+			
+			// Get tweets from Home Timeline
+			getHomeTimeLine();
+
 		} else {
 			// print a message so we know when it finishes
 			System.out.println("Successfull");
 		}
 	}
 
-	private static Status sendTweet(Twitter twitter, String text) {
+	private static Status sendTweet(String text) {
+		// access the twitter API using your twitter4j.properties file
+		// The factory instance is re-useable and thread safe.
+		Twitter twitter = TwitterFactory.getSingleton();
+
 		Status status = null;
 		try {
 			status = twitter.updateStatus(text);
-			System.out.println("Tweet posted : " + text);
+			System.out.println("Successfully updated the status to ["
+					+ status.getText() + "].");
 		} catch (TwitterException e) {
 			e.printStackTrace();
 		}
 
 		return status;
+	}
+
+	private static void getHomeTimeLine() {
+		Twitter twitter = TwitterFactory.getSingleton();
+		List<Status> statuses = null;
+		try {
+			statuses = twitter.getHomeTimeline();
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Showing home timeline.");
+		if (statuses != null) {
+			for (Status status : statuses) {
+				System.out.println(status.getUser().getName() + ":"
+						+ status.getText());
+			}
+		}
 	}
 }
