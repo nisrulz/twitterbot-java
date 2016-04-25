4dddd436094d5ec4d66b13a5db86dbe245c76feb
Nishant Srivastava <nisrulz@gmail.com>
2016-04-25 12:56:33 +0530

    [+] added replyToTweet()

--- src/github/nisrulz/bot/TwitterBot.java
+++ src/github/nisrulz/bot/TwitterBot.java
@@ -5,6 +5,7 @@ import java.util.List;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -73,5 +74,26 @@ public class TwitterBot {
 			System.out.println("@" + status.getUser().getScreenName() + ":"
 					+ status.getText());
 		}
+	}
+
+	private static void replyToTweet(String query_text, String reply)
+			throws TwitterException {
+		// access the twitter API using your twitter4j.properties file
+		Twitter twitter = TwitterFactory.getSingleton();
+
+		// create a new search
+		Query query = new Query(query_text);
+
+		// get the results from that search
+		QueryResult result = twitter.search(query);
+
+		// get the first tweet from those results
+		Status tweetResult = result.getTweets().get(0);
+
+		// reply to that tweet
+		StatusUpdate statusUpdate = new StatusUpdate(".@"
+				+ tweetResult.getUser().getScreenName() + " " + reply);
+		statusUpdate.inReplyToStatusId(tweetResult.getId());
+		Status status = twitter.updateStatus(statusUpdate);
 	}
 }
